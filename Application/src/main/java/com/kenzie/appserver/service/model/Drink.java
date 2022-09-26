bc152830052e197e42f3c3dd33b80fa99ab4858d
Neha-s8 <neha.sharma@mykenzie.snhu.edu>
2022-09-25 21:50:06 -0700

    Drink and Drink services and unittest cases updated

--- Application/src/main/java/com/kenzie/appserver/service/model/Drink.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/Drink.java
@@ -27,6 +27,14 @@ public class Drink {
         this.userId = userId;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getId() {
         return id;
     }
