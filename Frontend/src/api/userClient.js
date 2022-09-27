cdae7753d99cf6d7a7e41937bcd3756baf3bd3fc
PAMELA HANDEL <pamela.handel@mykenzie.snhu.edu>
2022-09-27 10:53:10 -0700

    frontend shenanigans in userClient, css, and landing page

--- Frontend/src/api/userClient.js
+++ Frontend/src/api/userClient.js
@@ -107,3 +107,27 @@ export default class UserClient extends BaseClass {
         }
     }
 }
+
+//var checkList = document.getElementById('list1');
+//checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+//  if (checkList.classList.contains('visible'))
+//    checkList.classList.remove('visible');
+//  else
+//    checkList.classList.add('visible');
+//}
+//
+//var checkList = document.getElementById('list2');
+//checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+//  if (checkList.classList.contains('visible'))
+//    checkList.classList.remove('visible');
+//  else
+//    checkList.classList.add('visible');
+//}
+//
+//var checkList = document.getElementById('list3');
+//checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+//  if (checkList.classList.contains('visible'))
+//    checkList.classList.remove('visible');
+//  else
+//    checkList.classList.add('visible');
+//}
