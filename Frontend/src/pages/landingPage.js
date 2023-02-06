08a969564b5910d8df9d36f23f2d36f666ebbd5f
Jonathan Sydorowicz <90705257+jSydorowicz21@users.noreply.github.com>
2023-02-05 19:51:23 -0600

    Front end react (#17)
    * Basic front end react conversion
    
    * Migrated drinkPage.js to use react

--- Frontend/src/pages/landingPage.js
+++ Frontend/src/pages/landingPage.js
@@ -1,108 +1,78 @@
 import BaseClass from "../util/baseClass";
-import DataStore from "../util/DataStore";
 import DrinkClient from "../api/drinkClient";
 import UserClient from "../api/userClient";
+import getAndRenderDrinks from "../util/helperMethods";
+import { createRoot } from "react-dom/client";
+
+'use strict';
 
 /**
  * Logic needed for the view playlist page of the website.
  */
 class LandingPage extends BaseClass {
-
     constructor() {
         super();
         this.bindClassMethods(['onGet', 'onGetAllDrinks', 'onCreate',  'onGetUserDrinks', 'onGetFiltered'], this);
-        this.dataStore = new DataStore();
     }
 
     /**
-     * Once the page has loaded, set up the event handlers and fetch the concert list.
+     * Once the page has loaded, set up the event handlers and fet and render drink lists.
      */
     async mount() {
         document.getElementById('createButton').addEventListener('click', this.onCreate);
         document.getElementById('homeButton').addEventListener('click', this.onGetAllDrinks);
         document.getElementById('filterButton').addEventListener('click', this.onGetFiltered);
+
         this.client = new DrinkClient();
         this.userClient = new UserClient();
-
-        await this.onGetAllDrinks();
-        await this.onGetUserDrinks();
-
+        this.onGetAllDrinks();
+        this.onGetUserDrinks();
     }
 
-
     // Event Handlers --------------------------------------------------------------------------------------------------
-
     async onGet(event) {
-        // Prevent the page from refreshing on form submit
         event.preventDefault();
-
         let ingredients = document.getElementById("ingredient-string").value;
-        //this.dataStore.set("drinks", null);
-
         let result = await this.client.getFilteredDrink(ingredients, this.errorHandler);
-        this.dataStore.set("drinks", result);
+
         if (result) {
             this.showMessage(`Got ${result.name}!`)
-
         } else {
             this.errorHandler("Error doing GET!  Try again...");
-
-
         }
         document.querySelector('input[name="filter"]:checked').checked = false;
-
     }
 
     async onGetAllDrinks() {
-
-        let result = await this.client.getHomeDrinks();
-        this.renderDrinks(result);
-
+        console.log("Getting all drinks");
+        const className = 'drink';
+        const drinks = await this.client.getHomeDrinks();
+        const root = createRoot(document.getElementById("drink-list"));
+        getAndRenderDrinks(root, drinks, className);
     }
 
     async onGetUserDrinks() {
-        console.log(sessionStorage.getItem("userId"));
+        console.log("Started getting drinks for user: " + sessionStorage.getItem("userId"));
+        const className = 'side-drink';
+
         let drinks = await this.userClient.getUsersDrinks(sessionStorage.getItem("userId"));
+        const sidebar = createRoot(document.getElementById("sidebar"));
 
         if (drinks == null){
             document.getElementById("sidebar").innerHTML = "";
         }
         else {
-            let htmlToInsert = "";
-
-            drinks.forEach(function (drink) {
-                htmlToInsert += `<div class="side-drink">
-                <h4><b>Drink Name: ${drink.name}</b></h4>
-                <p>Ingredients: ${drink.ingredients}</p>
-            </div>`
-            });
-            document.getElementById("sidebar").innerHTML = htmlToInsert;
-
-            var drinkCards = Array.from(document.getElementsByClassName('side-drink'));
-
-            drinkCards.forEach(function (drinkCard, index) {
-                drinkCard.addEventListener('click', function () {
-                    sessionStorage.setItem("drinkId", drinks[index].id);
-                    sessionStorage.setItem("drinkName", drinks[index].name);
-                    sessionStorage.setItem("ingredients", drinks[index].ingredients);
-                    window.location.href = "/drink.html";
-                });
-            });
+            getAndRenderDrinks(sidebar, drinks, className);
         }
-
     }
 
     async onCreate(event) {
-        // Prevent the page from refreshing on form submit
         event.preventDefault();
-        this.dataStore.set("drink", null);
+        console.log("Starting drink creation");
 
         let name = document.getElementById("create-name-field").value;
-
         let userId = sessionStorage.getItem("userId");
-
         let ingredients = document.getElementById("create-form").querySelectorAll('input:checked');
-
         let ingredientsArray = [];
 
         for (let i = 0; i < ingredients.length; i++) {
@@ -111,25 +81,20 @@ class LandingPage extends BaseClass {
 
         this.showMessage(`Creating ${name}!`);
         const createdDrink = await this.client.createDrink(userId, name, ingredientsArray, this.errorHandler);
-        this.dataStore.set("drink", createdDrink);
 
         if (createdDrink) {
             this.showMessage(`Created ${createdDrink.name}!`)
-
+            await this.onGetAllDrinks();
         } else {
             this.errorHandler("Error creating!  Try again...");
         }
-
-        await this.onGetAllDrinks();
     }
 
     async onGetFiltered(event) {
-        // Prevent the page from refreshing on form submit
         event.preventDefault();
-        this.dataStore.set("drink", null);
-
+        console.log("Started filtering drinks")
         let ingredients = document.getElementById("filter-form").querySelectorAll('input:checked');
-
+        const className = 'drink';
         let ingredientsArray = [];
 
         for (let i = 0; i < ingredients.length; i++) {
@@ -137,54 +102,20 @@ class LandingPage extends BaseClass {
         }
 
         this.showMessage(`Searching for matches...`);
-
         const drinks = await this.client.getFilteredDrink(ingredientsArray, this.errorHandler);
 
         if (drinks) {
             this.showMessage(`Matches found, see drinks box!`);
+            const root = createRoot(document.getElementById("drink-list"));
+            getAndRenderDrinks(root, Array.from(drinks), className);
         } else {
             this.errorHandler("Unable to find matches, please refine search.");
         }
-
-        this.renderDrinks(drinks);
     }
-
-    renderDrinks(result){
-        let drinks = Array.from(result);
-        document.getElementById("result").innerHTML = "";
-        if (result) {
-            var htmlToInsert = "";
-
-            drinks.forEach(function (drink) {
-                htmlToInsert += `<div class="drink">
-                <h4><b>Drink Name: ${drink.name}</b></h4>
-                <p>Ingredients: ${drink.ingredients}</p>
-            </div>`
-            });
-            document.getElementById("result").innerHTML = htmlToInsert;
-
-            var drinkCards = Array.from(document.getElementsByClassName('drink'));
-
-            drinkCards.forEach(function (drinkCard, index) {
-                drinkCard.addEventListener('click', function () {
-                    sessionStorage.setItem("drinkId", drinks[index].id);
-                    sessionStorage.setItem("drinkName", drinks[index].name);
-                    sessionStorage.setItem("ingredients", drinks[index].ingredients);
-                    window.location.href = "/drink.html";
-                });
-            });
-
-        } else {
-            this.errorHandler("Error doing GET!  Try again...");
-        }
-    }
-
 }
 
-
-
 /**
- * Main method to run when the page contents have loaded.
+ * Main method to run when the page contents have loaded. Has variable checking to force login and clear drinks.
  */
 const main = async () => {
     const landingPage = new LandingPage();
